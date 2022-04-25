31d5c3ed6791072b3b49ccf473825876c40f6397
Mohamedbahsidi <mmohamedoulbah>
2022-04-25 23:12:27 +0200

    first commit

---
+++ src/Execution.java
@@ -0,0 +1,14 @@
+
+public class Execution implements Runnable {
+	PrincipalW mafenetre =new PrincipalW();
+	
+	
+	public static void main(String[] args) {
+      new Thread (new Execution()).start();
+	}
+public void run() {
+	while(true) {
+		mafenetre.repaint();
+	}
+}
+}
